3758d12f32088d126396f379a34d2203ae78a985
gustavo-nemeth <hidan_nemeth@hotmail.com>
2021-11-05 22:04:04 -0300

    First request

--- front/src/components/DonutChart/index.tsx
+++ front/src/components/DonutChart/index.tsx
@@ -1,11 +1,33 @@
 import Chart from 'react-apexcharts';
+import axios from "axios";
+import {BASE_URL} from "../../utils/request";
+import {SaleSum} from "../../types/sale";
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 function DonutChart() {
 
-    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
+    // ERRADO
+    let chartData : ChartData = { labels: [], series: [] }
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then( (response) => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+
+        chartData = { labels: myLabels, series: mySeries}
+        console.log(chartData)
+
+    })
+
+
+    // const mockData = {
+    //     series: [477138, 499928, 444867, 220426, 473088],
+    //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    // }
 
     const options = {
         legend: {
@@ -15,7 +37,7 @@ function DonutChart() {
 
     return (
         <div>
-            <Chart options={{...options, labels: mockData.labels}} series={mockData.series} type="donut" height="240"/>
+            <Chart options={{...options, labels: chartData.labels}} series={chartData.series} type="donut" height="240"/>
         </div>)
 }
 
--- front/src/pages/Dashboard/index.tsx
+++ front/src/pages/Dashboard/index.tsx
@@ -7,33 +7,31 @@ import React from "react";
 
 const Dashboard = () => {
     return (
-            <>
-                <NavBar/>
-                <div className="container">
-                    <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <>
+            <NavBar/>
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-                    <div className="row px-3">
-                        <div className="col-sm-6">
-                            <h5 className="text-center text-secondary">Taxa de sucesso</h5>
-                            <BarChart/>
-                        </div>
-                        <div className="col-sm-6">
-                            <h5 className="text-center text-secondary">Todas as vendas</h5>
-                            <DonutChart/>
-                        </div>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de sucesso</h5>
+                        <BarChart/>
                     </div>
-
-                    <div className="py-3">
-                        <h2 className="text-primary"> Todas as Vendas</h2>
-
-
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas as vendas</h5>
+                        <DonutChart/>
                     </div>
-                    <DataTable/>
                 </div>
-                <Footer/>
-            </>
+
+                <div className="py-3">
+                    <h2 className="text-primary"> Todas as Vendas</h2>
 
 
+                </div>
+                <DataTable/>
+            </div>
+            <Footer/>
+        </>
     )
 };
 
